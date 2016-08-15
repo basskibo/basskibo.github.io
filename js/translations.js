f3c243f64c85880d4d5032dfb2594b17b522b18e
basskibo <jageticb@yahoo.com>
2016-08-15 02:05:45 +0200

    modal and lots of things

--- js/translations.js
+++ js/translations.js
@@ -30,22 +30,30 @@
             en:'Contact Us',
             rs:'Kontaktirajte Nas'
         },
+        CONTACT_MORE:{
+            en:'If you want to hire us or have some questions, feel free to ask.',
+            rs:'Ukoliko zelite da nas unajmite ili imate neko pitanje, slobodno nas kontaktirajte.'
+        },
         NAME:{
             en:'Name: *',
             rs:'Ime: *'
         },
-	SEND_MESSAGE:{
-	    en: 'Send Message',
-	    rs: 'Posalji'
-	},
+        SEND_MESSAGE:{
+            en: 'Send Message',
+            rs: 'Posalji'
+        },
         GETSTARTED: {
             en: 'Get Started',
             rs: 'Saznaj više'
         },
-	EMAIL:{
-	    en: 'Email: *',
-	    rs: 'Email: *'
-	},
+        EMAIL:{
+            en: 'Email: *',
+            rs: 'Email: *'
+        },
+        EMAIL2:{
+                      en: 'Email',
+                      rs: 'Email'
+        },
         MESSAGE: {
             en:'Message: *',
             rs:'Poruka: *'
@@ -60,7 +68,6 @@
             en:'Featured work',
             rs:'Istaknuti radovi'
         },
-
         ALL: {
             en: 'All',
             rs: 'Sve'
@@ -110,18 +117,23 @@
             en: 'Address',
             rs: 'Adresa'
         },
-
-        
-
         PRICING: {
             en:'Pricing' ,
             rs:'Cenovnik'
         },
 
-	TELEPHONE: {
-	    en:'Telephone',
-	    rs:'Telefon'
-	}
+	    TELEPHONE: {
+	        en:'Telephone',
+	        rs:'Telefon'
+	    },
+	    PRICING_MORE:{
+	        en:'This is some of default pricing,if you want, you can contact us to make custom package.',
+	        rs:'Ovo su neke od uobicajnih cena, ukoliko zelite mozete nas kontaktirati da zajedno napravimo paket po vasoj zelji.'
+	    },
+	    SEE_MORE:{
+	        en: 'See more',
+	        rs: 'Vise detalja'
+	    }
 
 
 
